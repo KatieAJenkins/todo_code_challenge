5dca211d7e0f938a005a55971bf6cffce16c0b63
KatieAJenkins <kind2karma@gmail.com>
2017-02-06 19:00:56 -0700

    added calculator program

---
+++ calculator.js
@@ -0,0 +1,39 @@
+'use strict';
+
+function Calc(number1, number2) {
+  this.number1 = number1;
+  this.number2 = number2;
+};
+
+Calc.prototype.add = function add(number1, number2) {
+  // console.log(this.number1 + this.number2);
+  return this.number1 + this.number2;
+};
+
+Calc.prototype.subtract = function subtract (number1, number2) {
+  // console.log(this.number1 - this.number2);
+  return this.number1 - this.number2;
+};
+
+Calc.prototype.multiply = function multiply (number1, number2) {
+  // console.log(this.number1 * this.number2);
+  return this.number1 * this.number2;
+};
+
+Calc.prototype.divide = function divide (number1, number2) {
+  if (this.number1 === null || this.number2 === null) {
+    console.log("null");
+    return "null";
+  };
+  else {
+    console.log(this.number1 / this.number2);
+    return this.number1 / this.number2;
+  };
+};
+
+var newCalculation = new Calc (null, 20);
+
+newCalculation.add();
+newCalculation.subtract();
+newCalculation.multiply();
+newCalculation.divide();
--- scripts.js
+++ scripts.js
@@ -2,4 +2,19 @@
 
 (function () {
   console.log("JS is connected!");
+
+  $("#submitButton").on("click", function() {
+    console.log("clicked");
+  });
+
+  // function newItem (name, topic, dueDate, completed) {
+  //   const obj = items;
+  //   this.name = obj.name;
+  //   this.topic = obj.topic;
+  //   this.dueDate = obj.dueDate;
+  //   this.completed = obj.completed;
+  // }
+
+
+
 })();
